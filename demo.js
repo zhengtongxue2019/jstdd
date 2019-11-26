a2ccd89d5c57a5be94b585fd9adc0c5bbf79e748
郑少朋 <911917@nd>
2019-11-26 18:22:44 +0800

    提取函数，重命名变量，引入查询来代替临时变量

--- demo.js
+++ demo.js
@@ -21,27 +21,9 @@ function statement(invoice, plays){
 	let result = `Statement for ${invoice.customer}\n`;
 	const format = new Intl.NumberFormat("en-US", {style:"currency", currency:"USD", minimumFractionDigits:2}).format;
 	for (let perf of invoice.performances){
-			const play =  plays[perf.playId];
+			const play =  playFor(perf);
 			
-			let thisAmount = 0;
-	
-			switch (play.type) {
-				case "tradedy":
-					thisAmount = 40000;
-					if (perf.audience > 30) {
-						thisAmount += 1000 * (perf.audience - 30);
-					}
-					break;
-				case "comedy":
-					thisAmount = 30000;
-					if (perf.audience > 20) {
-						thisAmount += 1000 + 500 * (perf.audience - 20);
-					}
-					thisAmount += 300 * perf.audience;
-					break;
-				default:
-					throw new Error(`unknown type: ${play.type}`);
-			}
+			let thisAmount = amountFor(perf);
 			
 			//add volume credits
 			volumeCredits += Math.max(perf.audience - 30, 0);
@@ -57,6 +39,33 @@ function statement(invoice, plays){
 	result += `Amount owed is ${format(totalAmount/100)}\n`;
 	result += `You earned ${volumeCredits} credits\n`;
 	return result;
+
+	function playFor(perf) {
+		return plays[perf.playId];
+	}
+
+	function amountFor(perf) {
+		const play =  playFor(perf);
+		let thisAmount = 0;
+		switch (play.type) {
+			case "tradedy":
+				thisAmount = 40000;
+				if (perf.audience > 30) {
+					thisAmount += 1000 * (perf.audience - 30);
+				}
+				break;
+			case "comedy":
+				thisAmount = 30000;
+				if (perf.audience > 20) {
+					thisAmount += 1000 + 500 * (perf.audience - 20);
+				}
+				thisAmount += 300 * perf.audience;
+				break;
+			default:
+				throw new Error(`unknown type: ${play.type}`);
+		}
+		return thisAmount;
+	}
 }
 
 function calc(){
