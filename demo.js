7379750a9decc10212a8ad8f7ec7836fe55ce8c3
郑少朋 <911917@nd>
2019-11-26 18:47:49 +0800

    函数替代临时变量

--- demo.js
+++ demo.js
@@ -19,16 +19,24 @@ function statement(invoice, plays){
 
 	let result = `Statement for ${invoice.customer}\n`;
 
-	let totalAmount = 0;
+	let totalAmount = countTotalAmount();
+
 	for (let perf of invoice.performances){					
-			//print line for this order
-			result += `\t${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-			totalAmount += amountFor(perf);
+		//print line for this order
+		result += `\t${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
 	}
 	
 	result += `Amount owed is ${usd(totalAmount)}\n`;
 	result += `You earned ${totalVolumeCredits()} credits\n`;
 	return result;
+
+	function countTotalAmount() {
+		let totalAmount = 0;
+		for (let perf of invoice.performances) {
+			totalAmount += amountFor(perf);
+		}
+		return totalAmount;
+	}
 
 	function totalVolumeCredits() {
 		let result = 0;
