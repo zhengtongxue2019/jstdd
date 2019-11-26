dfcb62622a645475df81009af3ebfacb200c0cc1
郑少朋 <911917@nd>
2019-11-26 18:39:44 +0800

    更改更有意义的名字

--- demo.js
+++ demo.js
@@ -26,15 +26,15 @@ function statement(invoice, plays){
 	let result = `Statement for ${invoice.customer}\n`;
 	for (let perf of invoice.performances){					
 			//print line for this order
-			result += `\t${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
+			result += `\t${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience} seats)\n`;
 			totalAmount += amountFor(perf);
 	}
 	
-	result += `Amount owed is ${format(totalAmount/100)}\n`;
+	result += `Amount owed is ${usd(totalAmount/100)}\n`;
 	result += `You earned ${volumeCredits} credits\n`;
 	return result;
 
-	function format(aNumber) {
+	function usd(aNumber) {
 		return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber);
 	}
 
